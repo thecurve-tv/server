c2d0c7d06e03641ac43ac72562ac4e86fc4ab146
Victor <olaitanvictor99@gmail.com>
2021-06-15 16:00:56 -0400

    Add player guard

---
+++ src/graphql/player/contains-only-common-players.guard.ts
@@ -0,0 +1,33 @@
+import { ObjectId } from 'bson'
+import { IPlayer, Player } from '../../model/player'
+import { ResolverContext } from '../graphql'
+import { Guard, GuardInput, GuardOutput } from '../guard'
+import { FindManyArgs } from '../mongoose-resolvers'
+
+export default class ContainsOnlyCommonPlayersGuard extends Guard<ResolverContext, FindManyArgs, any> {
+  constructor() {
+    super('egress')
+  }
+  async check(
+    { context, data }: GuardInput<ResolverContext, FindManyArgs, any>
+  ): Promise<void | GuardOutput<FindManyArgs, any>> {
+    const players: IPlayer[] = data
+    // the game field might not have been requested, so fetch it
+    const playersGameIdsDoc = await Player.find({ _id: { $in: players.map(p => p._id) } }, { game: 1 })
+    const playerIdToGameIdMap = new Map(playersGameIdsDoc.map(player => {
+      return [player._id.toHexString(), (<ObjectId>player.game).toHexString()]
+    }))
+    // get ids of all the games requester has played in
+    const myGameIdsDoc = await Player.find({ account: context.account._id }, { game: 1 })
+    const myGameIds = new Set(myGameIdsDoc.map(player => (<ObjectId>player.game).toHexString()))
+    return {
+      data: players.filter(player => {
+        const playerIsOwnedByRequester = player.account && (<ObjectId>player.account).toHexString() == context.account._id.toHexString()
+        if (playerIsOwnedByRequester) return true
+        const playerGameId = playerIdToGameIdMap.get(player._id.toHexString())
+        // if the _id field wasn't requested we won't know the game id, so assert that it is there to pass
+        return playerGameId && myGameIds.has(playerGameId)
+      })
+    }
+  }
+}
