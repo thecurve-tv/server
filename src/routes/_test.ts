9f5337ae744f0049065f010335a17fc869d55a2c
Victor <olaitanvictor99@gmail.com>
2021-12-18 14:41:56 -0500

    Support e2e testing

---
+++ src/routes/_test.ts
@@ -0,0 +1,59 @@
+import { Router } from 'express'
+import { startSession } from 'mongoose'
+import { apolloServer } from '../graphql/graphql'
+import { Chat } from '../models/chat'
+import { ChatPlayer } from '../models/chatPlayer'
+import { Game } from '../models/game'
+import { Photo } from '../models/photo'
+import { Player } from '../models/player'
+import { Room } from '../models/room'
+import { AuthenticatedRequest, fetchAccount } from '../util/session'
+
+export const router = Router()
+
+router.post('/clearGames', fetchAccount(), (req: AuthenticatedRequest, res, next) => {
+  const accountId = req.account?._id
+  if (!accountId) return res.status(400).send('invalid account')
+  const clearGames = async (accountId: string) => {
+    const gameIds = (await Game.find(
+      { hostAccount: accountId },
+      { _id: 1 }
+    )).map(g => g._id)
+    const playerIds = (await Player.find(
+      { game: { $in: gameIds } },
+      { _id: 1 }
+    )).map(p => p._id)
+    const chatIds = (await Chat.find(
+      { game: { $in: gameIds } },
+      { _id: 1 }
+    )).map(c => c._id)
+    const session = await startSession()
+    await session.withTransaction(async () => {
+      await Promise.all([
+        Room.deleteMany({ player: { $in: playerIds } }, { session }),
+        Photo.deleteMany({ player: { $in: playerIds } }, { session }),
+        ChatPlayer.deleteMany({
+          $or: [
+            { chat: { $in: chatIds } },
+            { player: { $in: playerIds } }
+          ]
+        }, { session }),
+        Chat.deleteMany({ _id: { $in: chatIds } }, { session }),
+        Player.deleteMany({ _id: { $in: playerIds } }, { session }),
+        Game.deleteMany({ _id: { $in: gameIds } }, { session })
+      ])
+    })
+  }
+  Promise.all(req.body.accountIds.map(clearGames))
+    .then(() => res.sendStatus(200))
+    .catch(next)
+})
+
+router.post('/execGraphql', (req, res, next) => {
+  apolloServer.executeOperation(
+    req.body.request,
+    { account: req.body.account, req, res }
+  )
+    .then(gRes => res.send(gRes))
+    .catch(next)
+})
