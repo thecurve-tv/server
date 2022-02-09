8208ff5c0e64e10c7cba079edbda86ad7328e11d
Victor <olaitanvictor99@gmail.com>
2022-02-08 23:27:23 -0500

    Implicitly add cors

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(security.enableCors('thecurve.tv'))
+app.use(security.enableCors())
 
 app.use(graphqlUploadExpress())
 apolloServer.applyMiddleware({
